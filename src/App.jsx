12e7bb9fa1ab6faae8cc8f7b5c97138244ed1a45
christian calleja <christiandcalleja@yahoo.com.ar>
2022-03-29 11:18:28 -0300

    Iniciando proyecto 2

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+<div>
+
+<Header></Header>
+
+
+</div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+const Header = () => {
+  return (
+    <div>Header
+        <FontAwesomeIcon icon="fa-brands fa-steam-square" />
+    </div>
+  )
+}
+
+export default Header
