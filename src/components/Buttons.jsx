517ee5f0e8c6607bcd43054d53e8bd7028fc1584
christian calleja <christiandcalleja@yahoo.com.ar>
2022-03-30 11:04:36 -0300

    act

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,30 +1,27 @@
 import * as React from 'react'
-import { Button, ButtonGroup, makeStyles } from '@mui/material'
+import { Button, ButtonGroup} from '@mui/material'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUser } from "@fortawesome/free-solid-svg-icons"
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 
-const useStyles = makeStyles ({
-  buttonStyles: {
-    color: "green"
-  }
-});
 
-function alertClicked() {
-      alert('Sumaste un Item a tu carrito');
-    };
 
-export default function Buttons () {
-  const classes = useStyles();
+const Buttons = () => {
+
+  function alertClicked() {
+    alert('Sumaste un Item a tu carrito');
+  };
+
 
   return (
-        <ButtonGroup  variant="contained" aria-label="outlined primary button group">
-          <Button className={classes.buttonStyles}><FontAwesomeIcon icon={faMagnifyingGlass} /></Button>
+    <ButtonGroup  variant="contained" aria-label="outlined primary button group">
+          <Button ><FontAwesomeIcon icon={faMagnifyingGlass} /></Button>
           <Button><FontAwesomeIcon icon={faUser} /></Button>
           <Button><FontAwesomeIcon icon={faCartShopping} action onClick={alertClicked}/></Button>
         </ButtonGroup>
-      )
-    
-};
+  )
+}
+
+export default Buttons
