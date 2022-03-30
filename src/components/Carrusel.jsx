7f9bca5c5cf4e29c67ea73f59a693b01911f1c30
christian calleja <christiandcalleja@yahoo.com.ar>
2022-03-30 15:50:32 -0300

    carousel act

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -17,65 +17,63 @@ const Carrusel = () => {
 
 
   return (
-<div>
-<div id="carousel" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="10"></li>
+    <div>
+      <div id="carousel" className="carousel slide" data-ride="carousel">
+        <ul className="carousel-indicators">
+          <li data-target="#carousel" data-slide-to="0" className="active"></li>
+          <li data-target="#carousel" data-slide-to="1"></li>
+          <li data-target="#carousel" data-slide-to="2"></li>
+          <li data-target="#carousel" data-slide-to="3"></li>
+          <li data-target="#carousel" data-slide-to="4"></li>
+          <li data-target="#carousel" data-slide-to="5"></li>
+          <li data-target="#carousel" data-slide-to="6"></li>
+          <li data-target="#carousel" data-slide-to="7"></li>
+          <li data-target="#carousel" data-slide-to="8"></li>
+          <li data-target="#carousel" data-slide-to="9"></li>
 
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="100000">
-      <img class="img-thumbnail d-block mx-auto" src={deathstranding} alt="First slide" style={{height: 400}}/>
+
+        </ul>
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-bs-interval="100000">
+            <img className="img-thumbnail d-block mx-auto" src={deathstranding} alt="First slide" style={{ height: 400 }} />
+          </div>
+          <div className="carousel-item">
+            <img className="img-thumbnail d-block mx-auto" src={farmingsimulator} alt="Second slide" style={{ height: 400 }} />
+          </div>
+          <div className="carousel-item">
+            <img className="img-thumbnail d-block mx-auto" src={mecharmada} alt="Third slide" style={{ height: 400 }} />
+          </div>
+          <div className="carousel-item">
+            <img className="img-thumbnail d-block mx-auto" src={planetcrafter} alt="Fourth slide" style={{ height: 400 }} />
+          </div>
+          <div className="carousel-item">
+            <img className="img-thumbnail d-block mx-auto" src={planetside} alt="Fifth slide" style={{ height: 400 }} />
+          </div>
+          <div className="carousel-item">
+            <img className="img-thumbnail d-block mx-auto" src={pubg} alt="Sixth slide" style={{ height: 400 }} />
+          </div>
+          <div className="carousel-item">
+            <img className="img-thumbnail d-block mx-auto" src={rdr2} alt="Seventh slide" style={{ height: 400 }} />
+          </div>
+          <div className="carousel-item">
+            <img className="img-thumbnail d-block mx-auto" src={thehunter} alt="Eighth slide" style={{ height: 400 }} />
+          </div>
+          <div className="carousel-item">
+            <img className="img-thumbnail d-block mx-auto" src={twohospital} alt="Ninth slide" style={{ height: 400 }} />
+          </div>
+          <div className="carousel-item">
+            <img className="img-thumbnail d-block mx-auto" src={wartales} alt="Tenth slide" style={{ height: 400 }} />
+          </div>
+
+        </div>
+        <a className="carousel-control-prev" href="#carousel" data-slide="prev">
+          <span className="carousel-control-prev-icon"></span>
+        </a>
+        <a className="carousel-control-next" href="#carousel" data-slide="next">
+          <span className="carousel-control-next-icon"></span>
+        </a>
+      </div>
     </div>
-    <div class="carousel-item">
-      <img class="img-thumbnail d-block mx-auto" src={farmingsimulator} alt="Second slide" style={{height: 400}}/>
-    </div>
-    <div class="carousel-item">
-      <img class="img-thumbnail d-block mx-auto" src={mecharmada} alt="Third slide" style={{height: 400}}/>
-    </div>
-    <div class="carousel-item">
-      <img class="img-thumbnail d-block mx-auto" src={planetcrafter} alt="Fourth slide" style={{height: 400}}/>
-    </div>
-    <div class="carousel-item">
-      <img class="img-thumbnail d-block mx-auto" src={planetside} alt="Fifth slide" style={{height: 400}}/>
-    </div>
-    <div class="carousel-item">
-      <img class="img-thumbnail d-block mx-auto" src={pubg} alt="Sixth slide" style={{height: 400}}/>
-    </div>
-    <div class="carousel-item">
-      <img class="img-thumbnail d-block mx-auto" src={rdr2} alt="Seventh slide" style={{height: 400}}/>
-    </div>
-    <div class="carousel-item">
-      <img class="img-thumbnail d-block mx-auto" src={thehunter} alt="Eighth slide" style={{height: 400}}/>
-    </div>
-    <div class="carousel-item">
-      <img class="img-thumbnail d-block mx-auto" src={twohospital} alt="Ninth slide" style={{height: 400}}/>
-    </div>
-    <div class="carousel-item">
-      <img class="img-thumbnail d-block mx-auto" src={wartales} alt="Tenth slide" style={{height: 400}}/>
-    </div>
-  
-  </div>
-  <a class="carousel-control-prev" href="#carousel" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carousel" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-</div>
   )
 }
 
